6ef1412a452e46eeafdec3748b601b96203a5fa4
Derbs <derbs@debian.mh.rockynet.wireless.unca>
2012-09-05 00:41:18 -0400

    Initial for loop for project euler problem 1 working

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,14 @@
 using std::cout;
 using std::endl;
 
+//No longer does this say "Hello" to the world.  This program will now solve the first
+//Project Euler problem!  For reference, the task is rewritten here:
+//  Find the sum of all the multiples of 3 or 5 below 1000.
 
 int main() {
-  cout<<"Hello, world!"<<endl;
+  int sum = 0;
+  for(int i = 0; i<1000; ++i) {
+    sum += i;
+  }
+  cout << "The sum of the first 1000 integers is::" << sum << endl;
 }
