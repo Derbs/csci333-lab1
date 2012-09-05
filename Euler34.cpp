804412b2b430fe8118bea75ff9b204760413ba15
Derbs <derbs@debian.mh.rockynet.wireless.unca>
2012-09-05 01:05:08 -0400

    Created skeleton for Euler problem 34 solution. Updated make for Euler 34 as well.

---
+++ Euler34.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+//This program endeavors to solve project Euler problem 34, which is described as follows:
+
+//Find the sum of all numbers which are equal to the sum of the factorials 
+//of their digits.  1! and 2! are not included.
+
+//something to think about.  There is probably a practical limit where the factorials of the digits
+//MUST be greater than the original number.  Better find that or look for it as a condition.
+
+int factorial(int num);
+
+int main() {
+    
+}
--- Makefile
+++ Makefile
@@ -3,3 +3,5 @@ WARNINGS = -Wall -Wextra -Werror -pedantic -Weffc++
 
 hello: hello.cpp
 	$(COMP) -o hello hello.cpp $(WARNINGS)
+euler34: Euler34.cpp
+	$(COMP) -o euler34 Euler34.cpp $(WARNINGS)
